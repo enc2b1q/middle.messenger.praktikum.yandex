3448c272baedbc4c9c7307f168cf81b22dba7741
enc2b1q <93642807+enc2b1q@users.noreply.github.com>
2022-10-17 15:32:00 +0300

    added sccs

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ if (module.hot) {
     module.hot.accept();
 }
 
-// import './styles.css'; 
+// import './styles-native.css'; 
 
-import styles from './styles.css';
+import styles from './styles-native.css';
+
+import * as classes from './modules/style.module.scss';
+
+// document.body.className = classes.body;
 
 console.log('test');
 
