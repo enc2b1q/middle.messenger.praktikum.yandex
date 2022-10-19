9fe3eab9533411c85310bce9397e783c644250b5
enc2b1q <93642807+enc2b1q@users.noreply.github.com>
2022-10-19 23:26:04 +0300

    added chat selection page

---
+++ src/main/selectChat.js
@@ -0,0 +1,8 @@
+import tpl from './selectChat.hbs';
+import './selectChat.css';
+
+import layoutsidebar from '../layout/sidebar'
+
+const props = {
+};
+document.getElementById('root').innerHTML = tpl(props);
