472ac0f63a29805f15b624eedba2c50774878d81
enc2b1q <93642807+enc2b1q@users.noreply.github.com>
2022-11-19 21:45:48 +0300

    route + router

---
+++ src/services/Router.ts
@@ -0,0 +1,75 @@
+import Block from "./block";
+import Route from "./Route";
+
+class Router {
+
+    static __instance: Router;
+
+    routes: Array<Route>;
+    history: History;
+    _currentRoute: Route | null;
+    _rootQuery: string;
+
+    constructor(rootQuery: string) {
+        if (Router.__instance) {
+            return Router.__instance;
+        }
+
+        this.routes = [];
+        this.history = window.history;
+        this._currentRoute = null;
+        this._rootQuery = rootQuery;
+
+        Router.__instance = this;
+    }
+
+    use(pathname: string, block: typeof Block) {
+        const route = new Route(pathname, block, {rootQuery: this._rootQuery});
+        this.routes.push(route);
+        return this;
+    }
+
+    start() {
+        window.onpopstate = event => {
+            const pathname = event.currentTarget instanceof Window ? event.currentTarget.location.pathname : undefined;
+            if (pathname) {
+                this._onRoute(pathname);
+            }
+        };
+
+        this._onRoute(window.location.pathname);
+    }
+
+    _onRoute(pathname: string) {
+        const route = this.getRoute(pathname);
+        if (!route) {
+            return;
+        }
+
+        if (this._currentRoute) {
+            this._currentRoute.leave();
+        }
+
+        this._currentRoute = route;
+        route.render();
+    }
+
+    go(pathname: string) {
+        this.history.pushState({}, "", pathname);
+        this._onRoute(pathname);
+    }
+
+    back() {
+        this.history.back();
+    }
+
+    forward() {
+        this.history.forward();
+    }
+
+    getRoute(pathname: string) {
+        return this.routes.find(route => route.match(pathname));
+    }
+}
+
+export default Router;
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,13 +1,18 @@
-export type TypeDict<T> = {
+export type TypeDict<T = unknown> = {
     [key: string | number]: T
 }
 
 export type TypeDictUnknown = TypeDict<unknown>;
 
+export type TypeDictRootQuery<T = unknown> = TypeDict<T> & {
+    rootQuery: string;
+}
+
 export type TypeDefinedProps = {
     "setting"?: TypeDict<boolean | string>;
     "attr"?: TypeDict<string>;
     "events"?: TypeDict<EventListenerOrEventListenerObject>;
+
 };
 
 export type TypeMixedUnknownProps = TypeDefinedProps & TypeDictUnknown;
